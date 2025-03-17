febf642fadb9bd5828541e4217fee2d27cac4f06
TekuQT <teku6782@gmail.com>
2025-03-17 02:08:33 +0000

    Made a request class to add new requests and display the requests in order

---
+++ src/mainapp/Requests.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mainapp;
+
+/**
+ *
+ * @author tekuboii
+ */
+public class Requests {
+    //data members
+    private String name;
+    private int age;
+    private String priority;
+    private boolean patient;
+    private String gpDetails;
+    private Requests next;
+    
+    private static Requests head = null; //head of my linked list
+    //constructor
+    public Requests(String name, int age, String priority, boolean patient, String gpDetails) {
+        this.name = name;
+        this.age = age;
+        this.priority = priority;
+        this.patient = patient;
+        this.gpDetails = gpDetails;
+        this.next = null;
+    }
+    
+    // add requests at the end of the list
+        public static void addRequest(String name, int age, String priority, boolean patient, String gpDetails) {
+            Requests newRequest = new Requests (name, age, priority, patient, gpDetails);
+
+            if (head == null) {
+                head = newRequest;
+            } else {
+                Requests current = head;
+                while (current.next != null) {
+                    current = current.next;
+                }
+                current.next = newRequest;
+            }
+        }
+        //to display requests in order
+        //And if there is no requests it will diplay a different message
+        public static void displayRequests() {
+            if (head == null) {
+                System.out.println("No blood test requests.");
+                return;
+            }
+            Requests current = head;
+            while (current != null) {
+                System.out.println(current);
+                current = current.next;
+            }
+        }
+        
+    @Override
+    public String toString() {
+        return "Name: " + name + ", Age:" + age + ", Priority: "+ priority + ", " +(patient ? "Yes" : "No")+ ", GP: " + gpDetails;
+    }
+}
